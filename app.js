9173a9f7b948b9283d8504bbd122939d02799ec2
Geanfranco Dennis Vilca Salazar <105381806+Geanfranco5838@users.noreply.github.com>
2025-12-09 12:29:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,26 @@ function goTo(screen) {
     screens.forEach(s => s.classList.remove("active"));
     document.getElementById(screen).classList.add("active");
 }
+// Renderizar gráfico de asistencia
+document.addEventListener("DOMContentLoaded", function () {
+    const ctx = document.getElementById("chartAsistencia");
+
+    if (ctx) {
+        new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: ["Lun", "Mar", "Mié", "Jue", "Vie"],
+                datasets: [{
+                    label: "Asistencias",
+                    data: [28, 30, 31, 29, 32],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                plugins: { legend: { display: false } }
+            }
+        });
+    }
+});
+
